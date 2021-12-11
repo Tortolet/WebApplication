80d7ac7ac45543588299eeed1bec71e6491c576a
Huawei <ivanmineev52@gmail.com>
2021-12-11 15:56:18 +0300

    Подключение БД + изменение логина

---
+++ WebApplication/ChatContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebApplication
+{
+    public partial class ChatContext : DbContext
+    {
+        public ChatContext()
+        {
+        }
+
+        public ChatContext(DbContextOptions<ChatContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Login> Logins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Chat;Username=postgres;Password=14072003");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login>(entity =>
+            {
+                entity.HasKey(e => e.Log)
+                    .HasName("login_pkey");
+
+                entity.ToTable("login");
+
+                entity.Property(e => e.Log).HasColumnName("log");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Username).HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
