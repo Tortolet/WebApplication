fe0c8fe64203cb5cdeddcff4b7701b366a24d464
Huawei <ivanmineev52@gmail.com>
2021-12-25 22:19:08 +0300

    Сохранение сообщений в бд

--- WebApplication/ChatHub.cs
+++ WebApplication/ChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using WebApplication;
 
 namespace WebApplication
 {
@@ -8,6 +9,16 @@ namespace WebApplication
         public async Task Send(string message, string name)
         {
             await this.Clients.All.SendAsync("Send", message, name);
+
+            await Task.Run(() =>
+            {
+                using (ChatContext db = new ChatContext())
+                {
+                    Message msgToServ = new Message {Message1 = message, Username = name};
+                    db.Messages.Add(msgToServ);
+                    db.SaveChanges();
+                }
+            });
         }
     }
 }
